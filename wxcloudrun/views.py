36955e14afae57e10cb16fdcb1f75c18c795a6cc
李志强 <oncwnuB0oZ7he460Q7pIvSMQmYRA@git.weixin.qq.com>
2024-02-19 16:25:01 +0800

    处理pdf文件名 128k

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -225,7 +225,7 @@ def upload_pdf():
                 "role": "system",
                 "content": file_content,
             },
-            {"role": "user", "content": "充分阅读"+ filename +".pdf," +"提供20道选择题，并且给出对应的答案、解释、答案来源（具体到章节）。最后提供的是MySQL的执行脚本。MySQL数据库表是ask表，表字段分别是：question（问题）、"
+            {"role": "user", "content": "充分阅读"+ filename +".pdf," +"提供20道书中重要知识点相关的选择题，并且给出对应的答案、解释、答案来源（具体到章节）。最后提供的是MySQL的执行脚本。MySQL数据库表是ask表，表字段分别是：question（问题）、"
                                         + "optio_a(选项A)、option_b(选项B)、option_c(选项C)、option_d(选项D)、answer（答案，单选A或B或C或D）、explain（答案分析解释、知识点复述）、source（答案来源，具体到哪一章哪一节）。并且提供给表添加选择题的记录的sql脚本"},
         ]
 
@@ -235,7 +235,7 @@ def upload_pdf():
             messages=messages,
             temperature=0.3,
         )
-        app.logger.info(completion.choices[0])
+        app.logger.info(completion.choices[0].message.content)
         app.logger.info(completion)
 
         # 返回成功消息和文件路径
