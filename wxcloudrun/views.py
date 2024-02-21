7fa99a42a054f63d0dc8487cb3134c425cd0034b
李志强 <oncwnuB0oZ7he460Q7pIvSMQmYRA@git.weixin.qq.com>
2024-02-21 23:21:23 +0800

    总结

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -480,19 +480,19 @@ def upload_pdf_v1():
             {"role": "user",
              "content": "你是一个老师，请针对" + filename + ".pdf" + "的后面一半部分的内容，提供5道书中重要知识点相关的选择题，返回的格式要求：list的json字符串格式，list里面包含map，每个map包含这些属性：question（问题）、option_a(选项A,内容开头含A.)、option_b(选项B,内容开头含B.)、option_c(选项C,内容开头含C.)、option_d(选项D,内容开头含D.)、answer（答案，单选A或B或C或D）、fenxi（答案分析解释、知识点复述）、source（答案来源，具体到哪一章哪一节，不包含pdf文件名）"}
         ]
-        #
-        # messages3 = [
-        #     {
-        #         "role": "system",
-        #         "content": "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一些涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。",
-        #     },
-        #     {
-        #         "role": "system",
-        #         "content": file_content,
-        #     },
-        #     {"role": "user",
-        #      "content": "你是一个老师，请针对" + filename + ".pdf" + "的后三分之一部分的内容，提供5道书中重要知识点相关的选择题，返回的格式要求：list的json字符串格式，list里面包含map，每个map包含这些属性：question（问题）、option_a(选项A,内容开头含A.)、option_b(选项B,内容开头含B.)、option_c(选项C,内容开头含C.)、option_d(选项D,内容开头含D.)、answer（答案，单选A或B或C或D）、fenxi（答案分析解释、知识点复述）、source（答案来源，具体到哪一章哪一节，不包含pdf文件名）"}
-        # ]
+
+        messages3 = [
+            {
+                "role": "system",
+                "content": "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一些涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。",
+            },
+            {
+                "role": "system",
+                "content": file_content,
+            },
+            {"role": "user",
+             "content": "你是一个老师，请认真阅读" + filename + ".pdf" + "的全部内容，最终返回的是json的字符串。先总结该文档的内容（字数控制在20字内，返回字段是zongjie），然后按该文档的章节或组成部分来详细总结（返回的字段是fendian），"}
+        ]
 
         # 然后调用 chat-completion, 获取 kimi 的回答
         completion1 = client.chat.completions.create(
@@ -546,13 +546,19 @@ def upload_pdf_v1():
         # --------------------------3-------------------------------
 
         # 然后调用 chat-completion, 获取 kimi 的回答
-        # completion3 = client.chat.completions.create(
-        #     model="moonshot-v1-32k",
-        #     messages=messages3,
-        #     temperature=0.3,
-        # )
-        # # 第一次返回的答案
-        # text3 = completion3.choices[0].message.content
+        completion3 = client.chat.completions.create(
+            model="moonshot-v1-32k",
+            messages=messages3,
+            temperature=0.3,
+        )
+
+        text3 = completion3.choices[0].message.content;
+        record3 = Records()
+        record3.remark = text3
+        record3.remark2 = "zongjie"
+        record3.created_at = datetime.now()
+        insert_records(record3)
+
         # # 使用正则表达式匹配 ```json 和 ``` 之间的内容
         # pattern = re.compile(r"```json(.*?)```", re.DOTALL)
         # matches = pattern.findall(text3)
