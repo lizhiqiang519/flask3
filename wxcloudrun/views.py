e6087d366c51e30e334547b77117df7034217a7a
李志强 <oncwnuB0oZ7he460Q7pIvSMQmYRA@git.weixin.qq.com>
2024-02-22 22:50:31 +0800

    sk-IaFmuC7stozqOvRGTLlwPFLOsX

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -663,7 +663,7 @@ def upload_pdf_vip():
 
         client = OpenAI(
             # api_key="sk-nFhPcpNc2oBTxAMn7XP5KuL8ldxAKq9SFCky7xeCJzwqwkLV",sk-vdjbCMxXv762YrwrVxdZFWtC2DxrjE3BMOuVtEczmX5afvgV
-            api_key="sk-vdjbCMxXv762YrwrVxdZFWtC2DxrjE3BMOuVtEczmX5afvgV",
+            api_key="sk-IaFmuC7stQNyYEh63CJVeo94aqwrD2FozqOvRGTLlwPFLOsX",
             base_url="https://api.moonshot.cn/v1",
         )
 
