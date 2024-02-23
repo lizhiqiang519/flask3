76a7b688560e0f767d5cce4c9b045d37e668916d
李志强 <oncwnuB0oZ7he460Q7pIvSMQmYRA@git.weixin.qq.com>
2024-02-23 16:03:21 +0800

    file_content

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -991,7 +991,7 @@ def api_pdf_v1():
         calculate_token_response = requests.post(
             f'{MOONSHOT_API_URL}/tokenizers/estimate-token-count',
             headers=headers,
-            json={'model': 'moonshot-v1-128k', 'messages': [{'role': 'system', 'content': file_content.join()}]}
+            json={'model': 'moonshot-v1-128k', 'messages': [{'role': 'system', 'content': file_content}]}
         )
 
         app.logger.info("计算token结果 %s", calculate_token_response)
